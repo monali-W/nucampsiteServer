09f65da4ce896cb66e43a91c6c30457370c3f502
Monali-W <wasekarmonaligmail.com>
2021-09-08 01:17:32 -0400

    week 2 Assignment done

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
-
 const mongoose = require("mongoose");
 
+const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
