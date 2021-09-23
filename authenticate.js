ee0735860c1139055843aa63a467c4352d2fd7d4
Monali-W <wasekarmonaligmail.com>
2021-09-22 23:52:37 -0400

    facebook Auth

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ const User = require("./models/user");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
+const FacebookTokenStrategy = require("passport-facebook-token");
 
 const config = require("./config.js");
 
@@ -44,3 +45,38 @@ exports.verifyAdmin = function (req, res, next) {
     return next(err);
   }
 };
+console.log(config);
+exports.facebookPassport = passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(56, profile);
+      User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+          console.log("A");
+          return done(err, false);
+        }
+        if (!err && user) {
+          console.log("B");
+          return done(null, user);
+        } else {
+          console.log("C");
+          user = new User({ username: profile.displayName });
+          user.facebookId = profile.id;
+          user.firstname = profile.name.givenName;
+          user.lastname = profile.name.familyName;
+          user.save((err, user) => {
+            if (err) {
+              return done(err, false);
+            } else {
+              return done(null, user);
+            }
+          });
+        }
+      });
+    }
+  )
+);
--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite"
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "4410292079009266",
+    clientSecret: "2adc81eadd6afb175e0f4e9edd60c190"
+  }
 };
+// 4635967523120996;
